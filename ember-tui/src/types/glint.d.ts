1685daaee4358e247deb981e86d035963b0e40de
Patrick Pircher <patrick.pircher@ibm.com>
2025-12-02 20:11:07 +0100

    fix

--- ember-tui/src/types/glint.d.ts
+++ ember-tui/src/types/glint.d.ts
@@ -1,18 +1,5 @@
-import type ElementNode from '../dom/nodes/ElementNode';
-import '@glint/ember-tsc/types/-private/dsl/globals.d.ts';
-import '@glint/template/-private/dsl/emit';
-import type { ComponentLike, ModifierLike } from '@glint/template';
-import 'ember-modifier';
-import type {
-  EmptyObject,
-  NamedArgs,
-  PositionalArgs,
-} from 'ember-modifier/-private/signature';
-import type {
-  FunctionBasedModifier,
-  Teardown,
-} from 'ember-modifier/-private/function-based/modifier';
-import { ElementForTagName, MathMlElementForTagName, SVGElementForTagName } from "@glint/template/-private/dsl/types";
+import 'ember-source/types';
+
 
 declare module '*.gts' {
   import Component from '@glimmer/component';
