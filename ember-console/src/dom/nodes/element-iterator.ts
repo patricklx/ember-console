c4e86e42046f5e5ced1539d61f4d46d007fc6dc5
Patrick Pircher <patrick.pircher@ibm.com>
2025-12-02 11:46:06 +0100

    fix missing iterator

---
+++ ember-console/src/dom/nodes/element-iterator.ts
@@ -0,0 +1,8 @@
+
+
+export function* elementIterator(el: any): Generator<ElementNode, void, undefined> {
+  yield el;
+  for (const child of el.childNodes) {
+    yield* elementIterator(child);
+  }
+}
